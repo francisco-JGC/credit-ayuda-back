dff9e6d07d59a1ed799a89c509af87ebcd10c90b
francisco-JGC <jjjchico1@gmail.com>
2024-10-15 15:34:56 -0600

    feat: update Client and  assign route

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -87,3 +87,66 @@ export const deleteClientById = async (
     return handleError(error.message)
   }
 }
+
+export const updateClientById = async (
+  client: ICreateClient,
+  id: number
+): Promise<IHandleResponseController<Client>> => {
+  try {
+    const clientExist = await AppDataSource.getRepository(Client).findOne({
+      where: { id },
+      relations: ['route']
+    })
+
+    if (!clientExist) {
+      return handleNotFound('Cliente no encontrado')
+    }
+
+    AppDataSource.getRepository(Client).merge(clientExist, client)
+    const updatedUser =
+      await AppDataSource.getRepository(Client).save(clientExist)
+
+    await assignRouteToClient({ client_id: id, route_name: client.route_name })
+
+    return handleSuccess(updatedUser)
+  } catch (error: any) {
+    return handleError(error.message)
+  }
+}
+
+export const assignRouteToClient = async ({
+  client_id,
+  route_name
+}: {
+  client_id: number
+  route_name: string
+}): Promise<IHandleResponseController<Client>> => {
+  try {
+    const client = await AppDataSource.getRepository(Client).findOne({
+      where: { id: client_id },
+      relations: ['route']
+    })
+
+    if (!client) {
+      return handleNotFound('Cliente no encontrado')
+    }
+
+    if (client.route.name === route_name) {
+      return handleSuccess(client)
+    }
+
+    const { data: route, success } = await getRouteByName(route_name)
+
+    if (!success) {
+      return handleNotFound('No se encontro una ruta con ese nombre')
+    }
+
+    client.route = route as Route
+
+    await AppDataSource.getRepository(Client).save(client)
+
+    return handleSuccess(client)
+  } catch (error: any) {
+    return handleError(error.message)
+  }
+}
