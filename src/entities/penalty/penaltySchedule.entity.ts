34674886257520cfd06334cd2286100f8fa4e795
francisco-JGC <jjjchico1@gmail.com>
2024-10-18 00:18:34 -0600

    feat: create penaltyPaymentSchedule entitty

---
+++ src/entities/penalty/penaltySchedule.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import { PenaltyPlan } from './penaltyPlan.entity'
+
+@Entity()
+export class PenaltyPaymentSchedule {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'date' })
+  dueDate: Date
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount_due: number
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+  amount_paid: number
+
+  @Column({ type: 'varchar', length: 50, default: 'pending' })
+  status: string
+
+  @ManyToOne(
+    () => PenaltyPlan,
+    (penaltyPlan) => penaltyPlan.penalty_payment_schedules
+  )
+  penalty_plan: PenaltyPlan
+}
