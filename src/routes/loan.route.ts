20c59604cf775a8618b46c2de871620631aefe7d
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-12 18:01:13 -0600

    feat: add loan requests retrieval and update loan functionality

--- src/routes/loan.route.ts
+++ src/routes/loan.route.ts
@@ -3,7 +3,9 @@ import {
   createLoan,
   getLoanById,
   getLoans,
-  getPaginationLoans
+  getPaginationLoans,
+  getRequests,
+  updateLoan
 } from '../controllers/loan.controller'
 import { LoanFrequency, LoanStatus } from '../entities/loan/types/loan'
 import { authorizeRoles } from '../middlewares/authorizeRoles.middleware'
@@ -39,6 +41,7 @@ router.get(
     const limitNumber = parseInt(limit, 10)
     const frequency = (req.query.frequency) as LoanFrequency | undefined
     const status = req.query.status as LoanStatus | undefined
+    const statuses = req.query.statuses as LoanStatus[] | undefined
     const route = req.query.route as string | undefined
 
     return res.json(
@@ -48,6 +51,30 @@ router.get(
         dni: filter,
         frequency,
         status,
+        statuses,
+        route
+      })
+    )
+  }
+)
+
+router.get(
+  '/requests/:page/:limit/:filter?',
+  isAuth,
+  authorizeRoles(['admin']),
+  async (req, res) => {
+    const { page, limit, filter } = req.params
+    const pageNumber = parseInt(page, 10)
+    const limitNumber = parseInt(limit, 10)
+    const frequency = (req.query.frequency) as LoanFrequency | undefined
+    const route = req.query.route as string | undefined
+
+    return res.json(
+      await getRequests({
+        page: pageNumber,
+        limit: limitNumber,
+        dni: filter,
+        frequency,
         route
       })
     )
@@ -73,5 +100,9 @@ router.get(
     )
   }
 )
+
+router.put('/update', isAuth, authorizeRoles(['admin']), async (req, res) => {
+  return res.json(await updateLoan(req.body))
+})
 
 export default router
