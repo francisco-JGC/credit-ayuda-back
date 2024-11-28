b466cb930d422a86dea096825f1e9c6f753bf100
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-27 18:01:47 -0600

    feat: add endpoint to retrieve payment details by ID

--- src/routes/payments.route.ts
+++ src/routes/payments.route.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
-import { updatePaymentSchedule } from '../controllers/payment-schedule.controller'
+import { getPayment, updatePaymentSchedule } from '../controllers/payment-schedule.controller'
 import { isAuth } from '../middlewares/isAuth.middleware'
 import { authorizeRoles } from '../middlewares/authorizeRoles.middleware'
 
 const router = Router()
 
+router.get('/:id', isAuth, authorizeRoles(['admin']), async (req, res) => {
+  const id = parseInt(req.params.id)
+  return res.json(await getPayment(id))
+})
+
 router.post('/update/', isAuth, authorizeRoles(['admin', 'inventory']), async (req, res) => {
   return res.json(await updatePaymentSchedule(req.body))
 })
+
 export default router
