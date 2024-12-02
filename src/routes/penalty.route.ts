aba52ea55e5e6a43229ab7fca0f4b2a105df07a8
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-02 17:14:16 -0600

    feat: add routes for retrieving a penalty plan by ID and adding a penalty payment

--- src/routes/penalty.route.ts
+++ src/routes/penalty.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createPenaltyPlan, getPenaltyPlans } from '../controllers/penalty.controller'
+import { addPenaltyPayment, createPenaltyPlan, getPenaltyPlan, getPenaltyPlans } from '../controllers/penalty.controller'
 import { isAuth } from '../middlewares/isAuth.middleware'
 import { authorizeRoles } from '../middlewares/authorizeRoles.middleware'
 
@@ -10,10 +10,23 @@ router.get('/', isAuth, authorizeRoles(['admin', 'seller']), async (_, res) => {
   return res.json(penaltyPlans)
 })
 
+router.get('/:id', isAuth, authorizeRoles(['admin', 'seller']), async (req, res) => {
+  const id = parseInt(req.params.id)
+  const penaltyPlan = await getPenaltyPlan(id)
+  return res.json(penaltyPlan)
+})
+
 router.post('/', isAuth, authorizeRoles(['admin', 'seller']), async (req, res) => {
   const penaltyPlan = req.body
   const savedPlan = await createPenaltyPlan(penaltyPlan)
   return res.json(savedPlan)
+})
+
+router.post('/add-payment/:id', isAuth, authorizeRoles(['admin', 'seller']), async (req, res) => {
+  const id = parseInt(req.params.id)
+  const penaltyPayment = req.body
+  const updatedPlan = await addPenaltyPayment(id, penaltyPayment)
+  return res.json(updatedPlan)
 })
 
 export default router
