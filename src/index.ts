9c3ec3448d1a7c0b6baf9045b7dd8856481c300e
francisco-JGC <jjjchico1@gmail.com>
2024-12-14 16:34:52 -0600

    fix:

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,9 @@ import { createDefaultRoles } from './controllers/initializers/role.initializer'
 
 const app = express()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(
-  cors({
-    origin: '*'
-  })
-)
 
 const port = process.env.PORT || 3001
 
