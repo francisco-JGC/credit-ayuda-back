b48612660bf31780c996e7fd8a564aa0ff15d8da
francisco-JGC <jjjchico1@gmail.com>
2024-12-14 20:12:38 -0600

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import 'dotenv/config'
 
-import express, { Router } from 'express'
+import express from 'express'
 import cors from 'cors'
 import * as fs from 'fs'
 import * as path from 'path'
+import { AppDataSource } from './config/database.config'
+import { createDefaultRoles } from './controllers/initializers/role.initializer'
 
 const app = express()
-const router = Router()
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(
   cors({
     origin: '*'
   })
 )
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+
+const port = process.env.PORT || 3001
 
 fs.readdirSync(path.join(__dirname, 'routes')).map(async (file) => {
   const { default: route } = await import(`./routes/${file}`)
@@ -22,8 +25,21 @@ fs.readdirSync(path.join(__dirname, 'routes')).map(async (file) => {
   app.use(`/api/${routeName}`, route)
 })
 
-router.get('/', function (_req, res) {
-  res.send('Hellow world')
-})
+async function main() {
+  try {
+    await AppDataSource.initialize()
+    await runInitializers()
 
-export default app
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`)
+    })
+  } catch (error: any) {
+    console.log(error.message)
+  }
+}
+
+async function runInitializers() {
+  return await Promise.all([createDefaultRoles()])
+}
+
+main()
