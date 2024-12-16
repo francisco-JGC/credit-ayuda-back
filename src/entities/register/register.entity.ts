1e0610483b7e2cc6edb9df905fadb0dfd1458baf
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-16 00:26:24 -0600

    Add register entity with fields for transaction types and user association

---
+++ src/entities/register/register.entity.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { User } from '../user/user.entity'
+
+@Entity('register')
+export class Register {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'enum', enum: ['income', 'withdraw', 'loan', 'savings', 'cash'], default: 'income' })
+  type: 'income' | 'withdraw' | 'loan' | 'savings' | 'cash'
+
+  @Column({ nullable: true, type: 'text' })
+  details?: string
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount: number
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  cash?: number
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  savings?: number
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  withdraw?: number
+
+  @ManyToOne(() => User, (user) => user.registers, { onDelete: 'SET NULL' })
+  user: User
+
+  @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date
+}
