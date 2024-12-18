ff70bf191de6a00cab189d8009e9849978042889
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-18 11:55:50 -0600

    feat: add isAllowedTime middleware for time-based authorization

--- src/middlewares/authorizeRoles.middleware.ts
+++ src/middlewares/authorizeRoles.middleware.ts
@@ -1,3 +1,4 @@
+import { format, toZonedTime } from 'date-fns-tz'
 import { Response, Request, NextFunction } from 'express'
 import { verify } from 'jsonwebtoken'
 
@@ -26,7 +27,7 @@ export const authorizeRoles = (allowedRoles: string[]) => {
     }
 
     try {
-      const decoded = verify(token, process.env.JWT_SECRET as string) as {
+      const decoded = verify(token, process.env.JWT_SECRET!) as {
         role: string
       }
       req.user = decoded
@@ -42,5 +43,46 @@ export const authorizeRoles = (allowedRoles: string[]) => {
     } catch (error) {
       return res.json({ success: false, message: 'Token inválido o expirado' })
     }
+  }
+}
+
+export const isAllowedTime = () => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const authorizationHeader = req.header('Authorization') || ''
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      return res.json({ success: false, message: 'Autorización requerida' })
+    }
+
+    const token = authorizationHeader.replace('Bearer ', '')
+
+    if (!token) {
+      return res.json({ success: false, message: 'No estás autorizado' })
+    }
+
+    const decoded = verify(token, process.env.JWT_SECRET!) as {
+      role: string
+    }
+    // Define la zona horaria
+    const timeZone = 'America/Guatemala'
+    const now = new Date()
+
+    // Convierte la hora a la zona horaria deseada
+    const zonedDate = toZonedTime(now, timeZone)
+    // Formatea la hora
+    const hour = +format(zonedDate, 'HH', { timeZone })
+    if (decoded.role === 'admin') {
+      return next()
+    }
+
+    if (hour >= 8 && hour <= 18) {
+      return next()
+    }
+
+    return res.json({ success: false, message: 'hour_expired' })
   }
 }
