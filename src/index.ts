337646049aa3d1ef6dadb35ebff7bc3b60e1d862
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-12-18 11:56:08 -0600

    fix: reorder import statements for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
 import { AppDataSource } from './config/database.config'
